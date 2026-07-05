3f20e2e9f572e1370775f57fafe9639142a7d8d4
schoolprobuddy <schoolprobuddy@gmail.com>
2026-07-05 14:38:15 +1200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -20,6 +20,15 @@ export const metadata = {
     url: "https://talkaph.com",
     siteName: "talkaPH",
   },
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({ children }) {
